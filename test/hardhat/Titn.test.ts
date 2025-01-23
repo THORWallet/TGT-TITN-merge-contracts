4b0ccbf279c5e92037208077e9233078c40183c8
mattgabriel <matt.g.martire@gmail.com>
2025-01-23 16:29:24 +0100

    bridge back test

--- test/hardhat/Titn.test.ts
+++ test/hardhat/Titn.test.ts
@@ -88,4 +88,53 @@ describe('Titn tests', function () {
         expect(finalBalanceA).eql(initialAmount.sub(tokensToSend))
         expect(finalBalanceB).eql(tokensToSend)
     })
+    it('should bridge tokens back from ARB to BASE', async function () {
+        const tokensToSend = ethers.utils.parseEther('1')
+        // Defining extra message execution options for the send operation
+        const options = Options.newOptions().addExecutorLzReceiveOption(200000, 0).toHex().toString()
+        const sendParam = [
+            eidB,
+            ethers.utils.zeroPad(ownerB.address, 32),
+            tokensToSend,
+            tokensToSend,
+            options,
+            '0x',
+            '0x',
+        ]
+        // Fetching the native fee for the token send operation
+        const [nativeFee] = await baseTITN.quoteSend(sendParam, false)
+        // Executing the send operation from TITN contract
+        await baseTITN.send(sendParam, [nativeFee, 0], ownerA.address, { value: nativeFee })
+
+        // Fetch ownerB's initial balance on arbTITN
+        const initialBalanceB = await arbTITN.balanceOf(ownerB.address)
+        expect(initialBalanceB).to.eql(ethers.utils.parseEther('1')) // Confirm ownerB has 1 TITN on arbTITN
+
+        // Define the amount to send back and construct the parameters for the send operation
+        const tokensToSendBack = ethers.utils.parseEther('1') // Sending back the entire balance
+        const optionsBack = Options.newOptions().addExecutorLzReceiveOption(200000, 0).toHex().toString()
+        const sendBackParam = [
+            eidA, // Base endpoint ID
+            ethers.utils.zeroPad(ownerB.address, 32), // Sending back to ownerA's address on Base
+            tokensToSendBack, // Amount to send
+            tokensToSendBack, // Minimum amount to send
+            optionsBack, // Additional execution options
+            '0x', // Call data (none in this case)
+            '0x', // Call data (none in this case)
+        ]
+
+        // // Fetch the native fee for bridging the tokens back
+        const [nativeFeeBack] = await arbTITN.quoteSend(sendBackParam, false)
+
+        // // Execute the bridging operation from arbTITN to baseTITN
+        await arbTITN.connect(ownerB).send(sendBackParam, [nativeFeeBack, 0], ownerB.address, { value: nativeFeeBack })
+
+        // // Fetch final balances on arbTITN and baseTITN
+        const finalBalanceB = await arbTITN.balanceOf(ownerB.address)
+        const finalBalanceBOnBase = await baseTITN.balanceOf(ownerB.address)
+
+        // Assert balances after the bridging operation
+        expect(finalBalanceB).to.eql(ethers.utils.parseEther('0')) // ownerB should have no tokens left on arbTITN
+        expect(finalBalanceBOnBase.toString()).to.eql(tokensToSendBack.toString()) // ownerA should receive 1 TITN back on baseTITN
+    })
 })
