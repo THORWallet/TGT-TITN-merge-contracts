e09e19fa155e70f655305583932bf9fceef6f2bd
mattgabriel <matt.g.martire@gmail.com>
2025-03-21 14:44:20 +0100

    add splitter contract + mock USDC

---
+++ contracts/ERC20Splitter.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20Splitter is Ownable {
+    address[2] public recipients;
+    uint256[2] public percentages;
+    IERC20 public token;
+
+    event RecipientsUpdated(address[2] recipients, uint256[2] percentages);
+    event TokenUpdated(address token);
+    event Distributed(uint256 amount);
+    event TokensWithdrawn(address token, uint256 amount);
+    event ETHWithdrawn(uint256 amount);
+
+    constructor() Ownable(msg.sender) {}
+
+    function setRecipients(address[2] memory _recipients, uint256[2] memory _percentages) external onlyOwner {
+        require(_recipients[0] != address(0), "At least one recipient required");
+        uint256 totalPercent = _percentages[0] + _percentages[1];
+        require(totalPercent == 100, "Percentages must sum to 100");
+
+        recipients = _recipients;
+        percentages = _percentages;
+        emit RecipientsUpdated(_recipients, _percentages);
+    }
+
+    function setToken(address _token) external onlyOwner {
+        require(_token != address(0), "Invalid token address");
+        token = IERC20(_token);
+        emit TokenUpdated(_token);
+    }
+
+    function distribute() external {
+        require(address(token) != address(0), "Token not set");
+        uint256 balance = token.balanceOf(address(this));
+        require(balance > 0, "No tokens to distribute");
+
+        for (uint256 i = 0; i < 2; i++) {
+            if (recipients[i] != address(0) && percentages[i] > 0) {
+                uint256 amount = (balance * percentages[i]) / 100;
+                token.transfer(recipients[i], amount);
+            }
+        }
+        emit Distributed(balance);
+    }
+
+    function withdrawTokens(address _token) external onlyOwner {
+        require(_token != address(token), "Cannot withdraw primary token");
+        IERC20 otherToken = IERC20(_token);
+        uint256 balance = otherToken.balanceOf(address(this));
+        require(balance > 0, "No tokens to withdraw");
+        otherToken.transfer(owner(), balance);
+        emit TokensWithdrawn(_token, balance);
+    }
+
+    function withdrawETH() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No ETH to withdraw");
+        payable(owner()).transfer(balance);
+        emit ETHWithdrawn(balance);
+    }
+}
---
+++ contracts/mocks/Usdc.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Usdc is ERC20, Ownable {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _delegate,
+        uint256 initialMintAmount
+    ) ERC20(_name, _symbol) Ownable(_delegate) {
+        _mint(_delegate, initialMintAmount);
+    }
+}
